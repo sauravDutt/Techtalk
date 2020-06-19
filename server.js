c47d638321f8afd692517d7d74352a14d4f2f01a
sauravDutt <sauravd069@gmail.com>
2020-06-19 13:48:11 +0530

    Modification

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, `Welcome ${user.username} to Ansul University`));
+    socket.emit('message', formatMessage(botName, `Welcome ${user.username} to SD techtalk`));
 
     // Broadcast when a user connects
     socket.broadcast
